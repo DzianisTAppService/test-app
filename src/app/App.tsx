59095ea87a2fb5676d532b452a6b6109314b2ca9
Tsialitsyn Dzianis <tereshkoda@gmail.com>
2022-03-25 06:25:03 +0100

    Added main structure in progress added prettier added Header, NavBar

---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from 'common/Header';
+import NavBar from 'common/NavBar';
+
+function App() {
+  return (
+    <div className='App'>
+      <Header />
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/common/NavBar/NavBar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+
+const NavBar = () => {
+  return (
+    <Box>
+      <Typography>NavBar</Typography>
+    </Box>
+  );
+};
+
+export default NavBar;
