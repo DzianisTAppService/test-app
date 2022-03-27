cc2082d46652878ec4a6e8e20f3a677dec0bdd58
Tsialitsyn Dzianis <tereshkoda@gmail.com>
2022-03-27 17:17:31 +0200

    added SubscriptionForm.tsx: in progress

--- src/pages/SubscribePage/SubscriptionForm/SubscriptionForm.tsx
+++ src/pages/SubscribePage/SubscriptionForm/SubscriptionForm.tsx
@@ -1,25 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Controller, useForm } from 'react-hook-form';
-import { Box, Button, TextField } from '@mui/material';
+import {
+  Box,
+  Button,
+  Divider,
+  FormControl,
+  FormControlLabel,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Radio,
+  RadioGroup,
+  Select,
+} from '@mui/material';
+
+import { organizations, users } from 'testData';
+
+type OrganizationType = {
+  name: string;
+  id: string;
+};
 
 const SubscriptionForm = () => {
-  const { handleSubmit, reset, control } = useForm();
+  const { handleSubmit, reset, control, watch } = useForm();
+
+  const organizationValue = watch('organization');
 
   const onSubmit = (data: any) => console.log(data);
 
+  console.log(organizationValue, 'organization');
   return (
     <form>
-      <Controller
-        name={'textValue'}
-        control={control}
-        render={({ field: { onChange, value } }) => (
-          <TextField onChange={onChange} value={value} label={'Text Value'} />
-        )}
-      />
-      <Button onClick={handleSubmit(onSubmit)}>Submit</Button>
-      <Button onClick={() => reset()} variant={'outlined'}>
-        Reset
-      </Button>
+      <Grid container direction='column'>
+        <Grid item>
+          <Controller
+            name={'organization'}
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <FormControl fullWidth>
+                <InputLabel id='organization-label'>Organisation</InputLabel>
+                <Select labelId='organization-label' id='organization' onChange={onChange} value={organizationValue}>
+                  {(organizations as OrganizationType[]).map(({ name, id }) => (
+                    <MenuItem key={id} value={id}>
+                      <RadioGroup aria-label={name} name={name}>
+                        <FormControlLabel value={id} control={<Radio />} label={name} />
+                      </RadioGroup>
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            )}
+          />
+        </Grid>
+
+        <Box my={3}>
+          <Divider />
+        </Box>
+
+        <Grid item container justifyContent='flex-end'>
+          <Button onClick={handleSubmit(onSubmit)} variant='outlined'>
+            Submit
+          </Button>
+        </Grid>
+      </Grid>
     </form>
   );
 };
